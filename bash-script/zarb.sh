bd0764da33b17f668db0749c41cf453c540de675
Sina Haraeeni <s.haraeeni@sinacomsys.com>
2024-05-07 13:03:25 +0330

    Edit: Give env from user

--- bash-script/zarb.sh
+++ bash-script/zarb.sh
@@ -2,11 +2,11 @@
 # Created on: 2024/05/07
 # Last modify: 2024/05/07
 # Description: print matrix
-# Version: 1.0 
+# Version: 2.0
 
-for i in $(seq 1 3)
+for i in $(seq 1 $1)
 do
-    for j in $(seq 1 3)
+    for j in $(seq 1 $2)
     do
         let s=$i*$j
         echo -n "$i*$j=$s   "
