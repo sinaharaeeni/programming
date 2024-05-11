fbe73dc57f5dd66ec12405352c67b185eb99feec
Sina Haraeeni <s.haraeeni@sinacomsys.com>
2024-05-11 17:15:25 +0330

    Edit: print *

--- bash-script/zarb.sh
+++ bash-script/zarb.sh
@@ -2,14 +2,18 @@
 # Created on: 2024/05/07
 # Last modify: 2024/05/07
 # Description: print matrix
-# Version: 2.0
+# Version: 3.0
 
 for i in $(seq 1 $1)
 do
     for j in $(seq 1 $2)
     do
-        let s=$i*$j
-        echo -n "$i*$j=$s   "
+        if [ $i -eq $j ];then
+            echo -n "*****   "
+        else
+            let s=$i*$j
+            echo -n "$i*$j=$s   "
+        fi
     done
     echo ""
 done
